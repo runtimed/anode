de72014f26d38967ae520d553ce726f750880bd3
Kyle Kelley <rgbkrk@gmail.com>
2025-07-05 17:54:49 -0400

    toss max height on svg (#146)

--- src/components/outputs/SvgOutput.tsx
+++ src/components/outputs/SvgOutput.tsx
@@ -2,18 +2,13 @@ import React from "react";
 
 interface SvgOutputProps {
   content: string;
-  maxHeight?: string;
 }
 
-export const SvgOutput: React.FC<SvgOutputProps> = ({
-  content,
-  maxHeight = "400px",
-}) => {
+export const SvgOutput: React.FC<SvgOutputProps> = ({ content }) => {
   return (
     <div className="py-2">
       <div
         className="max-w-full overflow-hidden"
-        style={{ maxHeight }}
         dangerouslySetInnerHTML={{ __html: content }}
       />
     </div>
