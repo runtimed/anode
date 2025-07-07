37182ac0501d61df3d66d61e2731cf4d301cd1a2
Kyle Kelley <rgbkrk@gmail.com>
2025-07-07 12:09:19 -0400

    accept an ANODE_DEV_SERVER_PORT for the vite server (#155)
    Note: I didn't prefix with VITE_ so that it wouldn't get bundled on
    production builds.

--- vite.config.ts
+++ vite.config.ts
@@ -4,33 +4,39 @@ import { fileURLToPath } from "node:url";
 import { livestoreDevtoolsPlugin } from "@livestore/devtools-vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { visualizer } from "rollup-plugin-visualizer";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-export default defineConfig({
-  build: {
-    sourcemap: true,
-  },
-  server: {
-    port: 5173,
-  },
-  worker: { format: "es" },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    build: {
+      sourcemap: true,
     },
-  },
-  plugins: [
-    react(),
-    tailwindcss(),
-    livestoreDevtoolsPlugin({ schemaPath: "./schema.ts" }),
-    visualizer({
-      filename: "dist/bundle-analysis.html",
-      open: false,
-      gzipSize: true,
-      brotliSize: true,
-    }),
-  ],
+    server: {
+      port: env.ANODE_DEV_SERVER_PORT
+        ? parseInt(env.ANODE_DEV_SERVER_PORT)
+        : 5173,
+    },
+    worker: { format: "es" },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    plugins: [
+      react(),
+      tailwindcss(),
+      livestoreDevtoolsPlugin({ schemaPath: "./schema.ts" }),
+      visualizer({
+        filename: "dist/bundle-analysis.html",
+        open: false,
+        gzipSize: true,
+        brotliSize: true,
+      }),
+    ],
+  };
 });
