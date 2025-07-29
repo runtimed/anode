941ab6d489923b626b4cfe7ae245d0fe572cb0ac
Mark Miro <contact@markmiro.com>
2025-07-29 11:01:55 -0700

    update start-nb to read env var and print the runtime command it runs

--- scripts/start-nb.sh
+++ scripts/start-nb.sh
@@ -1,10 +1,13 @@
+# Source the .env file to get VITE_RUNTIME_COMMAND
+if [ -f ".env" ]; then
+  echo "Sourcing .env file"
+  source .env
+fi
+
 export DEV_PORT=${ANODE_DEV_SERVER_PORT:-5173}
 export NOTEBOOK_ID=$(date +%s)-$(openssl rand -hex 4)
-export LIVESTORE_SYNC_URL="ws://localhost:$DEV_PORT/livestore"
-echo "Starting runtime for notebook: $NOTEBOOK_ID on port: $DEV_PORT"
-echo "Sync URL: $LIVESTORE_SYNC_URL"
 
-URL="http://localhost:$DEV_PORT/?notebook=$NOTEBOOK_ID&_t=$(date +%s)"
+URL="http://localhost:$DEV_PORT/?notebook=$NOTEBOOK_ID"
 echo ""
 echo "=============================================="
 echo "✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨"
@@ -15,9 +18,27 @@ echo "✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨�
 echo "=============================================="
 echo ""
 
+echo "INCOGNITO: $ANODE_OPEN_INCOGNITO"
+
+
+sleep 3
+
+# Open browser
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  (sleep 1 && /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --incognito "$URL" &)
+  if [[ "$ANODE_OPEN_INCOGNITO" == "1" ]]; then
+    echo "Opening in incognito mode"
+    /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --incognito "$URL" &
+  else
+    echo "Opening in normal mode"
+    open "$URL" &
+  fi
 else
-  (sleep 1 && open "$URL" &)
+  open "$URL" &
 fi
-RUNT_LOG_LEVEL=debug deno run --allow-all --unstable-broadcast-channel --env-file=.env ../runt/packages/pyodide-runtime-agent/src/mod.ts
+
+RUNTIME_CMD="NOTEBOOK_ID=${NOTEBOOK_ID} ${VITE_RUNTIME_COMMAND}"
+echo "Starting runtime in background..."
+echo "\n\nRuntime command:\n\n$RUNTIME_CMD\n\n"
+
+# Start runtime in background
+eval $RUNTIME_CMD
