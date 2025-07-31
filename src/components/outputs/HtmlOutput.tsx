715b7579adbc9415b2a6bbbf04214793bade5cae
Mark Miro <contact@markmiro.com>
2025-07-31 14:42:23 -0700

    Fade in

--- src/components/outputs/HtmlOutput.tsx
+++ src/components/outputs/HtmlOutput.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useTimeout } from "react-use";
 import IframeOutput from "./IframeOutput";
 
 interface HtmlOutputProps {
@@ -11,18 +10,10 @@ interface HtmlOutputProps {
 export const HtmlOutput: React.FC<HtmlOutputProps> = ({
   content,
   className = "max-w-none dataframe-container",
-  delay = 0,
 }) => {
-  const [isReady] = useTimeout(delay);
-
-  // Don't render the iframe immediately - wait for the timeout
-  if (!isReady()) {
-    return <div className={className}>Loading...</div>;
-  }
-
   return (
     <IframeOutput
-      className={className}
+      className={`${className} fade-in`}
       content={String(content || "")}
       style={
         {
