f1e855ab868eeda4210997ebffd403a8e8a73754
Mark Miro <contact@markmiro.com>
2025-07-31 11:32:49 -0700

    Setup basic virtualizer, but don't use it

--- src/components/notebook/VirtualizedList.tsx
+++ src/components/notebook/VirtualizedList.tsx
@@ -1,4 +1,5 @@
-// import { useVirtualizer } from "@tanstack/react-virtual";
+import { useVirtualizer } from "@tanstack/react-virtual";
+import { useRef } from "react";
 
 function getRandomInt(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -7,14 +8,19 @@ function getRandomInt(min: number, max: number) {
 const ITEMS = Array.from({ length: 1000 }, (_, i) => getRandomInt(20, 300));
 
 export function VirtualizedList() {
-  // const rowVirtualizer = useVirtualizer({
-  //   count: ITEMS.length,
-  //   getScrollElement: () => document.body,
-  //   estimateSize: (i) => ITEMS[i],
-  // });
+  const parentRef = useRef(null);
+
+  const rowVirtualizer = useVirtualizer({
+    count: ITEMS.length,
+    getScrollElement: () => document.body,
+    estimateSize: (i) => ITEMS[i],
+  });
 
   return (
-    <div className="absolute top-0 left-0 h-screen w-screen overflow-scroll overscroll-contain border-2 border-red-500">
+    <div
+      ref={parentRef}
+      className="absolute top-0 left-0 h-screen w-screen overflow-scroll overscroll-contain border-2 border-red-500"
+    >
       VirtualizedList
       {ITEMS.map((item, i) => (
         <div
