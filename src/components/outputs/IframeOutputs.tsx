e5425ad1b92b4c1d9ed271bd70ee13a1ea5e0140
Mark Miro <contact@markmiro.com>
2025-08-06 10:17:35 -0700

    Fix grouping outputs

--- src/components/outputs/IframeOutputs.tsx
+++ src/components/outputs/IframeOutputs.tsx
@@ -1,18 +1,22 @@
 import { groupConsecutiveStreamOutputs } from "@/util/output-grouping";
 import { OutputData } from "@runt/schema";
 import { IframeOutput2 } from "./IframeOutput2";
-import ReactJsonView from "@microlink/react-json-view";
+import { useMemo } from "react";
 
 export const IframeOutputs = ({ outputs }: { outputs: OutputData[] }) => {
   // Apply grouping strategy based on cell type
-  const processedOutputs = outputs.sort(
-    (a: OutputData, b: OutputData) => a.position - b.position
+  const processedOutputs = useMemo(
+    () =>
+      groupConsecutiveStreamOutputs(
+        outputs.sort((a: OutputData, b: OutputData) => a.position - b.position)
+      ),
+    [outputs]
   );
 
   return (
     <div>
       {/* IframeOutputs: {processedOutputs.length} */}
-      <ReactJsonView src={processedOutputs} collapsed={1} />
+      {/* <ReactJsonView src={processedOutputs} collapsed={1} /> */}
       <IframeOutput2 outputs={processedOutputs} isReact />
       {/* {processedOutputs.map((output: OutputData, index: number) => (
         <div
