8e73939712e8a85e2b067f5ef3961801d0d5326d
Mark Miro <contact@markmiro.com>
2025-08-07 15:13:49 -0700

    Don't render json in iframe since we might already be in one

--- src/components/outputs/JsonOutput.tsx
+++ src/components/outputs/JsonOutput.tsx
@@ -1,5 +1,5 @@
+import ReactJsonView from "@microlink/react-json-view";
 import React from "react";
-import IframeOutput from "./IframeOutput";
 
 interface JsonOutputProps {
   data: unknown;
@@ -15,7 +15,20 @@ export const JsonOutput: React.FC<JsonOutputProps> = ({
 
   return (
     <div className="rounded bg-gray-50/50 p-2">
-      <IframeOutput isReact content={JSON.stringify(jsonData)} />
+      <ReactJsonView
+        src={jsonData}
+        theme="rjv-default"
+        collapsed={false}
+        displayDataTypes={false}
+        displayObjectSize={false}
+        enableClipboard={true}
+        indentWidth={2}
+        iconStyle="triangle"
+        style={{
+          backgroundColor: "transparent",
+          fontSize: "0.875rem",
+        }}
+      />
     </div>
   );
 };
