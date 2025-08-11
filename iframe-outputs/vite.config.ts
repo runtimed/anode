600830930a2543a55f45a8205c311c5dca1c9436
Mark Miro <contact@markmiro.com>
2025-08-11 09:59:40 -0700

    Remove index.html light bundle for now

--- iframe-outputs/vite.config.ts
+++ iframe-outputs/vite.config.ts
@@ -16,7 +16,6 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
         react: resolve(__dirname, "src/react.html"),
       },
     },
