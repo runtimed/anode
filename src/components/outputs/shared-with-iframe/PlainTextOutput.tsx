90477a458b8a68658d4f8f20322cc79415a4938a
Mark Miro <contact@markmiro.com>
2025-08-11 11:37:05 -0700

    Remove todos

--- src/components/outputs/shared-with-iframe/PlainTextOutput.tsx
+++ src/components/outputs/shared-with-iframe/PlainTextOutput.tsx
@@ -9,7 +9,6 @@ export const PlainTextOutput: React.FC<PlainTextOutputProps> = ({
   content,
   className = "font-mono text-sm whitespace-pre-wrap leading-relaxed text-gray-700",
 }) => {
-  // TODO: added stuff for testing
   return <div className={className}>{content}</div>;
 };
 
