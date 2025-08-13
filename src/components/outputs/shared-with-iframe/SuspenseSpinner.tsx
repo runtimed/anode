9d987c726635e53eecb1514a451b2de612ffbc32
Mark Miro <contact@markmiro.com>
2025-08-12 19:47:38 -0700

    Delay output spinner to avoid jank

--- src/components/outputs/shared-with-iframe/SuspenseSpinner.tsx
+++ src/components/outputs/shared-with-iframe/SuspenseSpinner.tsx
@@ -1,8 +1,17 @@
 import { Suspense } from "react";
 import { Spinner } from "../../ui/Spinner";
 
+import React from "react";
+import { useTimeout } from "react-use";
+
+function DelayedSpinner({ delay = 500 }) {
+  const [isReady] = useTimeout(delay);
+
+  return isReady() ? <LoadingSpinner /> : null;
+}
+
 export function SuspenseSpinner({ children }: { children: React.ReactNode }) {
-  return <Suspense fallback={<LoadingSpinner />}>{children}</Suspense>;
+  return <Suspense fallback={<DelayedSpinner />}>{children}</Suspense>;
 }
 
 export const LoadingSpinner = () => (
