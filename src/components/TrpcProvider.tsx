b7a8b70f4983fb8369077f3180ee9074f4ba7887
Mark Miro <contact@markmiro.com>
2025-08-21 16:52:34 -0700

    Add react query devtools in dev mode

--- src/components/TrpcProvider.tsx
+++ src/components/TrpcProvider.tsx
@@ -1,15 +1,24 @@
 import { trpcQueryClient } from "@/lib/trpc-client";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ReactNode } from "react";
 
 interface TrpcProviderProps {
   children: ReactNode;
 }
 
+/**
+ * 🚨 IMPORTANT: only include once in a page
+ *
+ * This is a wrapper component that provides the trpcQueryClient to the children.
+ * It also includes the ReactQueryDevtools for development.
+ *
+ */
 export const TrpcProvider = ({ children }: TrpcProviderProps) => {
   return (
     <QueryClientProvider client={trpcQueryClient}>
       {children}
+      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   );
 };
