36d2bfe1d46b856720e85f99a9a8e6be6b76c040
Mark Miro <contact@markmiro.com>
2025-08-28 12:44:35 -0700

    More room in cards

--- src/components/notebooks/NotebookDashboard.tsx
+++ src/components/notebooks/NotebookDashboard.tsx
@@ -587,7 +587,7 @@ const NotebookGrid: React.FC<NotebookGridProps> = ({
 }) => {
   if (viewMode === "grid") {
     return (
-      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="grid gap-6 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
         {notebooks.map((notebook) => (
           <NotebookCard
             key={notebook.id}
