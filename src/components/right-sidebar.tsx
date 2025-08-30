e25273016cf7be8e44916d36c1ae5c05839c23d9
Mark Miro <contact@markmiro.com>
2025-08-30 11:56:16 -0700

    Remove unused stuff in right sidebar

--- src/components/right-sidebar.tsx
+++ src/components/right-sidebar.tsx
@@ -1,9 +1,6 @@
 import * as React from "react";
 import { Plus } from "lucide-react";
 
-// import { Calendars } from "@/components/calendars";
-// import { DatePicker } from "@/components/date-picker";
-import { NavUser } from "@/components/nav-user";
 import {
   Sidebar,
   SidebarContent,
@@ -15,46 +12,21 @@ import {
   SidebarSeparator,
 } from "@/components/ui/sidebar";
 
-// This is sample data.
-const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
-  },
-  calendars: [
-    {
-      name: "My Calendars",
-      items: ["Personal", "Work", "Family"],
-    },
-    {
-      name: "Favorites",
-      items: ["Holidays", "Birthdays"],
-    },
-    {
-      name: "Other",
-      items: ["Travel", "Reminders", "Deadlines"],
-    },
-  ],
-};
-
 export function SidebarRight({
   ...props
 }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar
       collapsible="none"
-      className="sticky top-0 hidden h-svh border-l lg:flex"
+      className="sticky top-0 hidden h-svh lg:flex"
       {...props}
     >
       <SidebarHeader className="border-sidebar-border h-16 border-b">
-        <NavUser user={data.user} />
+        {/* <NavUser user={data.user} /> */}
       </SidebarHeader>
       <SidebarContent>
         Hello
-        {/* <DatePicker /> */}
         <SidebarSeparator className="mx-0" />
-        {/* <Calendars calendars={data.calendars} /> */}
       </SidebarContent>
       <SidebarFooter>
         <SidebarMenu>
