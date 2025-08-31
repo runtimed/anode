ecb1d312ed70afd37f206054768e6b8c2010a679
Mark Miro <contact@markmiro.com>
2025-08-31 16:42:51 -0700

    Same header size between dashboard and nb page

--- src/pages/NbPage.tsx
+++ src/pages/NbPage.tsx
@@ -52,7 +52,7 @@ export default function NbPage() {
       >
         <AppSidebarNb />
         <SidebarInset className="h-dvh overflow-y-scroll overscroll-contain">
-          <header className="bg-background sticky top-0 z-50 flex h-14 shrink-0 items-center gap-2 border-b px-2">
+          <header className="bg-background sticky top-0 z-50 flex h-14 shrink-0 items-center gap-2 border-b px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator
               orientation="vertical"
