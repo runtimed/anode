b1f9f57d0145802972b6119f2594438c65134b8c
Mark Miro <contact@markmiro.com>
2025-09-02 07:49:34 -0700

    Move NotebookPage and extract helpers

--- src/pages/NotebookPage.tsx
+++ src/pages/NotebookPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { TrpcProvider } from "@/components/TrpcProvider.tsx";
-import { NotebookPage as NotebookPageComponent } from "@/components/notebooks/NotebookPage.tsx";
+import { NotebookPage as NotebookPageComponent } from "@/components/notebooks/notebook/NotebookPage";
 
 export const NotebookPage: React.FC = () => {
   return (
