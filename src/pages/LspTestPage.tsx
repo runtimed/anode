bb106068138534ef0f70f405b25a578ace98991d
Mark Miro <contact@markmiro.com>
2025-09-10 13:04:07 -0700

    Add simple codemirror to LSP test page

--- src/pages/LspTestPage.tsx
+++ src/pages/LspTestPage.tsx
@@ -1,3 +1,36 @@
+import { useCodeMirror } from "@uiw/react-codemirror";
+import { useEffect, useRef, useState } from "react";
+import { baseExtensions } from "@/components/notebook/codemirror/baseExtensions";
+import { python } from "@codemirror/lang-python";
+
+const extensions = [...baseExtensions, python()];
+
+const defaultCode = `
+def add(a: int, b: int):
+  return a + b
+
+add(3, 6)
+`.trim();
+
 export const LspTestPage = () => {
-  return <div>LspTestPage</div>;
+  const editorRef = useRef<HTMLDivElement | null>(null);
+  const [value, setValue] = useState(defaultCode);
+
+  const { setContainer } = useCodeMirror({
+    container: editorRef.current,
+    extensions,
+    basicSetup: false,
+    maxHeight: "100%",
+    value,
+    onChange: setValue,
+    autoFocus: true,
+  });
+
+  useEffect(() => {
+    if (editorRef.current) {
+      setContainer(editorRef.current);
+    }
+  }, [setContainer]);
+
+  return <div ref={editorRef} />;
 };
