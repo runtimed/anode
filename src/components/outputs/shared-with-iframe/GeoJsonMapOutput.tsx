af6065a6d9017bcdce49e8089ce7bd11568ffacc
Mark Miro <contact@markmiro.com>
2025-09-11 16:41:05 -0700

    Remove "fixed" icons

--- src/components/outputs/shared-with-iframe/GeoJsonMapOutput.tsx
+++ src/components/outputs/shared-with-iframe/GeoJsonMapOutput.tsx
@@ -2,17 +2,6 @@ import { useEffect, useRef } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
-// Fix for default markers
-delete (L.Icon.Default.prototype as any)._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png",
-  iconUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png",
-  shadowUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
-});
-
 interface GeoJsonMapOutputProps {
   data: unknown;
 }
