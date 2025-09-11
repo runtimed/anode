0f28d1830bf11ad05416f03bc86ca642f2220810
Mark Miro <contact@markmiro.com>
2025-09-11 12:48:14 -0700

    Initial chat mode UI

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,16 @@ import { BrowserRouter } from "react-router-dom";
 
 import { App } from "@/routes.tsx";
 import { DebugProvider } from "@/components/debug/debug-mode.js";
+import { ChatModeProvider } from "@/hooks/useChatMode.tsx";
 
 ReactDOM.createRoot(document.getElementById("react-app")!).render(
   <React.StrictMode>
     <DebugProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ChatModeProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ChatModeProvider>
     </DebugProvider>
   </React.StrictMode>
 );
