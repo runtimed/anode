47d47870266f72328d59c351d015f714b695f758
Kyle Kelley <rgbkrk@gmail.com>
2025-09-12 08:57:00 -0700

    resolve packages locally

--- vite.config.ts
+++ vite.config.ts
@@ -109,6 +109,7 @@ export default defineConfig(({ mode }) => {
       alias: {
         "@": path.resolve(__dirname, "./src"),
       },
+      conditions: ["development", "browser", "import", "module", "default"],
     },
     optimizeDeps: {
       exclude: ["@livestore/wa-sqlite"],
