93397473f88af2393a823b45ced5603bb3e6e18b
Mark Miro <contact@markmiro.com>
2025-09-25 15:02:39 -0700

    Put cell betweener drop events in callbacks

--- src/components/notebook/cell/CellBetweener.tsx
+++ src/components/notebook/cell/CellBetweener.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 import { CellReference } from "@runtimed/schema";
 import { Plus } from "lucide-react";
 import { useAddCell } from "@/hooks/useAddCell.js";
-import { memo } from "react";
+import { memo, useCallback } from "react";
 import { useDragDropCellSort } from "@/hooks/useDragDropCellSort";
 
 export const CellBetweener = memo(function CellBetweener({
@@ -17,36 +17,47 @@ export const CellBetweener = memo(function CellBetweener({
   const { addCell } = useAddCell();
   const { setDraggingOverCell, onDrop } = useDragDropCellSort();
 
-  if (isDraggingOver) {
-    return (
-      <div className="group relative flex h-6 w-full items-center justify-center">
-        <div className="h-1 w-full bg-blue-500" />
-      </div>
-    );
-  }
+  const handleDragOver = useCallback(
+    (e: React.DragEvent) => {
+      e.preventDefault();
+      setDraggingOverCell(cell.id, position);
+    },
+    [cell.id, position, setDraggingOverCell]
+  );
+
+  const handleDrop = useCallback(
+    (e: React.DragEvent) => {
+      e.preventDefault();
+      onDrop(cell.id, position);
+    },
+    [cell.id, position, onDrop]
+  );
 
   return (
     <div
       className="group relative flex h-6 w-full items-center justify-center"
-      onDragOver={() => setDraggingOverCell(cell.id, position)}
-      onDrop={() => onDrop(cell.id, position)}
+      onDragOver={handleDragOver}
+      onDrop={handleDrop}
     >
-      <div className="absolute -left-[13px] z-10 flex h-px w-full items-center bg-transparent has-hover:bg-neutral-500">
-        <Button
-          variant="ghost"
-          size="xs"
-          className="bg-background text-gray-300 hover:bg-black hover:text-white"
-          onClick={() =>
-            addCell(
-              cell.id,
-              cell.cellType === "raw" ? "code" : cell.cellType,
-              position
-            )
-          }
-        >
-          <Plus />
-        </Button>
-      </div>
+      {isDraggingOver && <div className="h-1 w-full bg-blue-500" />}
+      {!isDraggingOver && (
+        <div className="absolute -left-[13px] z-10 flex h-px w-full items-center bg-transparent has-hover:bg-neutral-500">
+          <Button
+            variant="ghost"
+            size="xs"
+            className="bg-background text-gray-300 hover:bg-black hover:text-white"
+            onClick={() =>
+              addCell(
+                cell.id,
+                cell.cellType === "raw" ? "code" : cell.cellType,
+                position
+              )
+            }
+          >
+            <Plus />
+          </Button>
+        </div>
+      )}
     </div>
   );
 });
