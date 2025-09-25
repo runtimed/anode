ba81d08c671a2860f24d417bf0009ea207ed0ceb
Mark Miro <contact@markmiro.com>
2025-09-25 10:52:08 -0700

    Spruce up reorder demo

--- src/pages/ReorderDemoPage.tsx
+++ src/pages/ReorderDemoPage.tsx
@@ -1,5 +1,10 @@
 import { useAuthenticatedUser } from "@/auth";
 import { AuthGuard } from "@/auth/AuthGuard";
+import { cellTypeStyles } from "@/components/notebook/cell/CellTypeButtons";
+import { ExecutableCell } from "@/components/notebook/cell/ExecutableCell";
+import { RuntimePanel } from "@/components/notebook/sidebar-panels/RuntimePanel";
+import { useNotebook } from "@/components/notebooks/notebook/helpers";
+import { TitleEditor } from "@/components/notebooks/notebook/TitleEditor";
 import { SimpleUserProfile } from "@/components/notebooks/SimpleUserProfile";
 import { TrpcProvider, useTrpc } from "@/components/TrpcProvider";
 import { Button } from "@/components/ui/button";
@@ -7,6 +12,7 @@ import { useAddCell } from "@/hooks/useAddCell";
 import { CustomLiveStoreProvider } from "@/livestore";
 import { useQuery, useStore } from "@livestore/react";
 import {
+  cellTypes,
   events,
   moveCellBetween,
   queries,
@@ -14,6 +20,7 @@ import {
   tables,
 } from "@runtimed/schema";
 import { useMutation } from "@tanstack/react-query";
+import { Plus } from "lucide-react";
 import { useCallback } from "react";
 import { useLocalStorage } from "react-use";
 
@@ -21,7 +28,6 @@ export const ReorderDemoPage = () => {
   return (
     <div>
       <AuthGuard>
-        <SimpleUserProfile />
         <TrpcProvider>
           <InnerDemo />
         </TrpcProvider>
@@ -32,6 +38,7 @@ export const ReorderDemoPage = () => {
 
 function InnerDemo() {
   const trpc = useTrpc();
+
   const createNotebookMutation = useMutation(
     trpc.createNotebook.mutationOptions()
   );
@@ -66,60 +73,152 @@ function InnerDemo() {
   }
 
   return (
-    <div>
-      NB ID: {nbId}
-      <Button
-        onClick={async () => {
-          setNbId("");
-          await deleteNotebookMutation.mutateAsync({ nbId });
-        }}
-      >
-        Delete Nb
-      </Button>
+    <>
+      <div className="sticky top-0 flex items-center gap-2 border-b border-gray-200 bg-white p-2">
+        <NbTitle nbId={nbId} />
+        ID: <pre className="text-xs">{nbId}</pre>
+        <Button
+          size="xs"
+          variant="destructive"
+          onClick={async () => {
+            setNbId("");
+            await deleteNotebookMutation.mutateAsync({ nbId });
+          }}
+        >
+          Delete Nb
+        </Button>
+        <div className="flex-1" />
+        <SimpleUserProfile />
+      </div>
       <CustomLiveStoreProvider storeId={nbId}>
-        <NbDemo />
+        <AddCellButtons />
+        <div className="flex flex-col gap-2 p-2">
+          <div className="max-w-lg rounded-md border border-gray-200 p-4">
+            <RuntimePanelView nbId={nbId} />
+          </div>
+          <NbDemo />
+        </div>
       </CustomLiveStoreProvider>
-    </div>
+    </>
   );
 }
 
-function NbDemo() {
+function RuntimePanelView({ nbId }: { nbId: string }) {
+  const { notebook, isLoading, refetch } = useNotebook(nbId);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!notebook) {
+    return <div>Notebook not found</div>;
+  }
+
+  return <RuntimePanel notebook={notebook} onUpdate={refetch} />;
+}
+
+function NbTitle({ nbId }: { nbId: string }) {
+  const { notebook, isLoading, refetch } = useNotebook(nbId);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!notebook) {
+    return <div>Notebook not found</div>;
+  }
+
+  return (
+    <TitleEditor notebook={notebook} onTitleSaved={refetch} canEdit={true} />
+  );
+}
+
+const cellTypeStylesWithRaw = {
+  ...cellTypeStyles,
+  raw: "border-gray-300 focus-visible:border-gray-500 text-gray-600 hover:bg-gray-50 hover:text-gray-600 focus:bg-gray-50 focus-visible:ring-gray-100",
+};
+
+const cellReferences$ = queryDb(
+  tables.cells
+    .select("id", "fractionalIndex", "cellType", "source")
+    .orderBy("fractionalIndex", "asc"),
+  { label: "cells.withIndices" }
+);
+
+function AddCellButtons() {
+  const cellReferences = useQuery(cellReferences$);
   const userId = useAuthenticatedUser();
   const { store } = useStore();
-  const cellReferences = useQuery(
-    queryDb(
-      tables.cells
-        .select("id", "fractionalIndex", "cellType", "source")
-        .orderBy("fractionalIndex", "asc"),
-      { label: "cells.withIndices" }
-    )
-  );
   const moveLastCellToTop = useMoveLastCellToTop();
 
   const { addCell } = useAddCell();
 
   return (
+    <>
+      {/* Add cell buttons */}
+      <div className="sticky top-0 z-50 flex flex-wrap gap-1 border-b border-gray-200 bg-white p-2">
+        {cellTypes.map((cellType) => (
+          <Button
+            variant="outline"
+            className={cellTypeStylesWithRaw[cellType]}
+            key={cellType}
+            onClick={() => {
+              const cellId = addCell(undefined, cellType, "after");
+              store.commit(
+                events.cellSourceChanged({
+                  id: cellId,
+                  source: cellReferences.length.toString(),
+                  modifiedBy: userId,
+                })
+              );
+            }}
+          >
+            <Plus className="h-3 w-3" /> {cellType}
+          </Button>
+        ))}
+        <Button onClick={moveLastCellToTop}>Move Last Cell to Top</Button>
+      </div>
+    </>
+  );
+}
+
+function NbDemo() {
+  const cellReferences = useQuery(cellReferences$);
+
+  return (
     <div>
-      <Button
-        onClick={() => {
-          const cellId = addCell(undefined, "sql", "after");
-          store.commit(
-            events.cellSourceChanged({
-              id: cellId,
-              source: cellReferences.length.toString(),
-              modifiedBy: userId,
-            })
-          );
-        }}
-      >
-        Add Cell
-      </Button>
-      <Button onClick={moveLastCellToTop}>Move Last Cell to Top</Button>
-      {cellReferences.map((cell) => (
-        <div key={cell.id}>
-          <pre>{JSON.stringify(cell)}</pre>
-        </div>
-      ))}
+      {/* Cell list */}
+      <div className="relative flex flex-col gap-2">
+        {cellReferences.map((cell) => (
+          <div key={cell.id}>
+            <Cell cell={cell} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Cell({
+  cell,
+}: {
+  cell: {
+    id: string;
+    fractionalIndex: string | null;
+    cellType: string;
+    source: string;
+  };
+}) {
+  const cell2 = useQuery(queries.cellQuery.byId(cell.id));
+
+  if (!cell2) {
+    console.warn("Asked to render a cell that does not exist");
+    return null;
+  }
+
+  return (
+    <div>
+      {/* <pre>{JSON.stringify(cell)}</pre> */}
+      <ExecutableCell cell={cell2} />
     </div>
   );
 }
