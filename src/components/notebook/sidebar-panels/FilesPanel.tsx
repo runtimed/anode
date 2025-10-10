2c8e505babfa6b1aebfd00bdf1b340e855321672
Mark Miro <contact@markmiro.com>
2025-10-09 17:18:10 -0700

    Show demo data in file tree

--- src/components/notebook/sidebar-panels/FilesPanel.tsx
+++ src/components/notebook/sidebar-panels/FilesPanel.tsx
@@ -1,6 +1,63 @@
+import { queryDb } from "@livestore/livestore";
+import { useQuery } from "@livestore/react";
+import { tables } from "@runtimed/schema";
 import React from "react";
 import type { SidebarPanelProps } from "./types";
 
+import { Tree } from "@/components/ui/file-tree";
+import { SidebarMenu, SidebarProvider } from "@/components/ui/sidebar";
+
+const data2 = {
+  tree: [
+    [
+      "app",
+      [
+        "api",
+        ["hello", ["route.ts"]],
+        "page.tsx",
+        "layout.tsx",
+        ["blog", ["page.tsx"]],
+      ],
+    ],
+    [
+      "components",
+      ["ui", "button.tsx", "card.tsx"],
+      "header.tsx",
+      "footer.tsx",
+    ],
+    ["lib", ["util.ts"]],
+    ["public", "favicon.ico", "vercel.svg"],
+    ".eslintrc.json",
+    ".gitignore",
+    "next.config.js",
+    "tailwind.config.js",
+    "package.json",
+    "README.md",
+  ],
+};
+
 export const FilesPanel: React.FC<SidebarPanelProps> = () => {
-  return <div className="space-y-6">File contents</div>;
+  const files = useQuery(queryDb(tables.files.select()));
+  console.log(files);
+
+  return (
+    <div className="space-y-6">
+      <SidebarProvider>
+        <SidebarMenu>
+          {data2.tree.map((item, index) => (
+            <Tree key={index} item={item} />
+          ))}
+        </SidebarMenu>
+      </SidebarProvider>
+
+      {/* <Tree>
+        <File value="page.tsx">page.tsx</File>
+        {files.map((file) => (
+          <File value={file.id} key={file.id}>
+            {file.fileName}
+          </File>
+        ))}
+      </Tree> */}
+    </div>
+  );
 };
---
+++ src/components/ui/file-tree.tsx
@@ -0,0 +1,61 @@
+import { ChevronRight, File, Folder, Trash2 } from "lucide-react";
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import {
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+} from "@/components/ui/sidebar";
+import { Button } from "./button";
+
+export function Tree({ item }: { item: string | any[] }) {
+  const [name, ...items] = Array.isArray(item) ? item : [item];
+
+  if (!items.length) {
+    return (
+      <SidebarMenuButton
+        // isActive={name === "button.tsx"}
+        className="group/tree-item data-[active=true]:bg-transparent"
+      >
+        <File />
+        {name}
+        <div className="grow"></div>
+        <Button
+          size="xs"
+          variant="destructiveGhost"
+          className="hidden group-focus-within/tree-item:block group-hover/tree-item:block"
+        >
+          <Trash2 />
+        </Button>
+      </SidebarMenuButton>
+    );
+  }
+
+  return (
+    <SidebarMenuItem>
+      <Collapsible
+        className="group/collapsible [&[data-state=open]>button>svg:first-child]:rotate-90"
+        defaultOpen={name === "components" || name === "ui"}
+      >
+        <CollapsibleTrigger asChild>
+          <SidebarMenuButton>
+            <ChevronRight className="transition-transform" />
+            <Folder />
+            {name}
+          </SidebarMenuButton>
+        </CollapsibleTrigger>
+        <CollapsibleContent>
+          <SidebarMenuSub>
+            {items.map((subItem, index) => (
+              <Tree key={index} item={subItem} />
+            ))}
+          </SidebarMenuSub>
+        </CollapsibleContent>
+      </Collapsible>
+    </SidebarMenuItem>
+  );
+}
