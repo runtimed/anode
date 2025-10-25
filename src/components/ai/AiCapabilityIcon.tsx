0f9d367b5243035c568a87e737b73ae503e26119
Mark Miro <contact@markmiro.com>
2025-10-24 17:43:24 -0700

    Add AiCapabilityIcon component

---
+++ src/components/ai/AiCapabilityIcon.tsx
@@ -0,0 +1,43 @@
+import { SimpleTooltip } from "@/components/ui/tooltip";
+import { cn } from "@/lib/utils";
+import { AiModel } from "@/util/ai-models";
+import { type ModelCapability } from "@runtimed/agent-core";
+import { Brain, Eye, LucideIcon, Type, Wrench } from "lucide-react";
+
+const capabilityIconMap = {
+  completion: Type,
+  tools: Wrench,
+  vision: Eye,
+  thinking: Brain,
+} as const satisfies Record<ModelCapability, LucideIcon>;
+
+const capabilityTooltipMap = {
+  completion: "Basic text completion",
+  tools: "Function/tool calling",
+  vision: "Image understanding",
+  thinking: "Chain of thought reasoning",
+} as const satisfies Record<ModelCapability, string>;
+
+export function AiCapabilityIcon({
+  iconClassName,
+  model,
+  capability,
+}: {
+  iconClassName?: string;
+  model: AiModel;
+  capability: ModelCapability;
+}) {
+  const Icon = capabilityIconMap[capability];
+  return (
+    <SimpleTooltip content={capabilityTooltipMap[capability]}>
+      <div>
+        <Icon
+          className={cn(
+            iconClassName,
+            !model.capabilities.includes(capability) && "opacity-30"
+          )}
+        />
+      </div>
+    </SimpleTooltip>
+  );
+}
